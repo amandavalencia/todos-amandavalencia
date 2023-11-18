6a322957da2acfc09f94aa1c23da2cf1122ddf45
Amanda Valencia <amandaavalencia@hotmail.com>
2023-11-18 12:58:07 +0100

    added eventlistener

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,9 @@ const haveDone = [];
 const listContainer = document.getElementById("section#ToDo");
 const listDoneContainer = document.getElementById("section#Done");
 const toDoContainer = document.createElement("ul");
+
+function createHtmlForToDo(){
+toDoContainer.innerHTML="";
 for(let i = 0; i < toDoList.length; i++){
     const toDos = document.createElement("li");
    
@@ -20,21 +23,40 @@ for(let i = 0; i < toDoList.length; i++){
     toDos.addEventListener("click", () => {
         haveDone.push(toDoList[i]);
         console.log("klart: " +haveDone);
+        toDoList.splice(i,1);
+        createHtmlForToDo()
         createHtmlForDone()
         })
 }
+}
+createHtmlForToDo();
+
 const doneContainer = document.createElement("ul");
+
 function createHtmlForDone(){
     doneContainer.innerHTML="";
     for(let i = 0; i < haveDone.length; i++){
         const done = document.createElement("li");
-        
 
         doneContainer.className = "doneContainer";
         done.innerHTML = haveDone[i];
 
         doneContainer.appendChild(done);
         listDoneContainer.appendChild(doneContainer);
+
+        done.addEventListener("click", ()=>{
+            toDoList.push(haveDone[i]);
+            haveDone.splice(i,1);
+           
+            createHtmlForToDo();
+            createHtmlForDone();
+        })
     }
 }
 
+// function createHtmlForToDo(){
+//     toDoContainer.innerHTML="";
+//     for(let i = 0; i<toDoList.length;i++){
+
+//     }
+// }
