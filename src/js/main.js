a5ffdda5e893cc77bb08059eb98cfd96c129d3af
Amanda Valencia <amandaavalencia@hotmail.com>
2023-11-19 11:11:14 +0100

    styling and added js class

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,40 @@
 import "/src/css/style.scss";
 
+const toDoApp = document.getElementById("toDoApp");
+const toDoHeader = document.createElement("h1");
+toDoHeader.innerHTML="To-Do List &#x1F4DD";
+toDoApp.appendChild(toDoHeader);
 
-const toDoList = ["Sätta igång tvättmaskinen","Handla till maten","Hämta nycklarna på kontoret","Baka en tårta till Peter",];
+class Todo{ 
+    checkbox;
+    task;
+    constructor(checkbox, task){
+        this.task = task;
+        this.checkbox = checkbox;
+    }
+}
+const task1 = new Todo (true, "Sätta igång tvättmaskinen");
+const task2 = new Todo (true,"Handla till maten");
+const task3 = new Todo (true,"Hämta nycklarna på kontoret");
+const task4 = new Todo (true,"Baka en tårta till Peter");
+
+const toDoList = [task1, task2, task3, task4];
 const haveDone = [];
 
-const listContainer = document.getElementById("section#ToDo");
-const listDoneContainer = document.getElementById("section#Done");
+const listContainer = document.getElementById("section__ToDo")
+const listDoneContainer = document.getElementById("section__Done");
 const toDoContainer = document.createElement("ul");
 
 function createHtmlForToDo(){
 toDoContainer.innerHTML="";
 for(let i = 0; i < toDoList.length; i++){
     const toDos = document.createElement("li");
-   
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    toDos.appendChild(checkbox);
 
     toDoContainer.className = "toDoContainer";
-    toDos.innerHTML= toDoList[i];
+    toDos.innerHTML= toDoList[i].task;
     
     toDoContainer.appendChild(toDos);
     listContainer.appendChild(toDoContainer);
@@ -29,7 +48,7 @@ for(let i = 0; i < toDoList.length; i++){
         })
 }
 }
-createHtmlForToDo();
+createHtmlForToDo()
 
 const doneContainer = document.createElement("ul");
 
@@ -39,7 +58,7 @@ function createHtmlForDone(){
         const done = document.createElement("li");
 
         doneContainer.className = "doneContainer";
-        done.innerHTML = haveDone[i];
+        done.innerHTML = haveDone[i].task;
 
         doneContainer.appendChild(done);
         listDoneContainer.appendChild(doneContainer);
@@ -53,10 +72,3 @@ function createHtmlForDone(){
         })
     }
 }
-
-// function createHtmlForToDo(){
-//     toDoContainer.innerHTML="";
-//     for(let i = 0; i<toDoList.length;i++){
-
-//     }
-// }
